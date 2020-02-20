7bb450f5488645829f8284a492b2c6d0ac30ac4d
Nhu Nguyen <nhunguyen@chotot.vn>
2020-02-20 11:42:04 +0700

    Initial commit

---
+++ TestFastlane/TestFastlane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFastlane
+//
+//  Created by Nhu Nguyen on 2/20/20.
+//  Copyright © 2020 Nhu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
